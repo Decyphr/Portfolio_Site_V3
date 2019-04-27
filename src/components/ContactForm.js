77c32eee892849eef6cc2f417b38ffc2bc14fd43
Blake Hartman <blakeha94@gmail.com>
2019-04-27 14:08:10 -0600

    Added HandleChange function

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,12 +4,25 @@ import { FlexContainer } from './FlexContainer'
 import { TextInput, SubmitBtn } from './Inputs'
 
 class ContactForm extends Component {
+  state = {
+    email: '',
+    project: ''
+  }
+
+  handleChange = e => {
+    e.preventDefault()
+
+    const { name, value } = e
+    this.setState({ name: value })
+
+  }
+
   render() {
     return (
       <form name="contact" method="POST" data-netlify="true">
         <FlexContainer>
-          <TextInput type='email' name='email' placeholder='Email' value={(e) => e.target.value} />
-          <TextInput type='text' name='name' placeholder='Project Idea...' value={(e) => e.target.value}/>
+          <TextInput onChange={this.handleChange} type='email' name='email' placeholder='Email' value={this.state.email} />
+          <TextInput onChange={this.handleChange} type='text' name='project' placeholder='Project Idea...' value={this.state.project}/>
           <SubmitBtn type='submit'>Send It</SubmitBtn>
         </FlexContainer>
       </form>
